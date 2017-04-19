6b8fc4ad06f10bf785a263d92d2d4de1183064e3
Rachel Chiang <rchiang@cpp.edu>
2017-04-19 00:30:02 -0700

    Cleaned up the code and added high-level comments.

--- CS380-P2/Decoder.java
+++ CS380-P2/Decoder.java
@@ -1,62 +1,52 @@
+/*
+ * Rachel Chiang
+ * CS 380.01 Computer Networks
+ * Project 2: 4B/5B NRZI
+ */
 import java.util.HashMap;
 
+/**
+ * This class handles decoding a received stream of bytes that are represented
+ * as an array of integers and that themselves represent a 32-Byte signal of
+ * bits.
+ */
 public class Decoder
 {
+   /**
+    * The {@link #table} is a simple lookup table for the 5b->4b mapping. It is
+    * initialized in the constructor {@link #Decoder(int, double)} and is filled
+    * in {@link #populateTable()}. A HashMap data structure was selected for
+    * quick access, and the 5b value is the key because this is being translated
+    * from a 5b to the 4b.
+    */
    private HashMap<Integer, Byte> table;
-   private int[] original5B;
+   /**
+    * The {@link #new5B} array contains only the values 0 and 1. It is filled in
+    * {@link #findTrue5BSignal(int[])}, where the received encoded signal is
+    * translated from NRZI to the server's original 5b signal.
+    */
    private int[] new5B;
+   /**
+    * This is the baseline from the PhysLayerClient and the server's preamble.
+    * It is used in {@link #findTrue5BSignal(int[])} to determine whether a
+    * received Byte represents a 0 or 1.
+    */
    private double baseline;
    
-   public Decoder(int[] received, double baseline)
+   /**
+    * Constructor.
+    */
+   public Decoder(double baseline)
    {
       table = new HashMap<Integer, Byte>(16);
       populateTable();
-      original5B = received;
       this.baseline = baseline;
    }
    
-   public byte[] decode()
-   {
-      findTrue5BSignal();
-      printOG5B();
-      byte[] true4BSignal = new byte[32];
-      int j = 0;
-      
-      for (int i = 0; i < new5B.length; )
-      {
-         int left = 0;
-         int right = 0;
-         for (int bitShift = 4; bitShift >= 0; --bitShift)
-         {
-            left += new5B[i++] << bitShift;
-         }
-         for (int bitShift = 4; bitShift >= 0; --bitShift)
-         {
-            right += new5B[i++] << bitShift;
-         }
-         //System.out.print(Integer.toBinaryString(left) + " " + Integer.toBinaryString(right) + "; ");
-         byte leftByte = table.get(left);
-         byte rightByte = table.get(right);
-         true4BSignal[j++] = (byte) ((leftByte << 4) + rightByte);
-         //System.out.print(" " + String.format("%02X", true4BSignal[j-1]));
-         
-         /*
-         // _XXXX
-         // _XXXX
-         left += new5B[i++] << 4;
-         // X_XXX
-         left += new5B[i++] << 3;
-         // XX_XX
-         left += new5B[i++] << 2;
-         // XXX_X
-         left += new5B[i++] << 1;
-         // XXXX_
-         left += new5B[i];*/
-      }
-      
-      return true4BSignal;
-   }
-   
+   /**
+    * This method simply populates the {@link #table} for looking up the 4b
+    * mapping that corresponds with a 5b signal.
+    */
    private void populateTable()
    {
       int[] fiveBitCode = {
@@ -67,24 +57,76 @@ public class Decoder
          table.put(fiveBitCode[i], (byte) i);
       }
    }
-   
-   private void findTrue5BSignal()
+
+   /**
+    * This method is called outside of the Decoder class. It begins the
+    * procedure for decoding a received signal. It calls on a handful of helper
+    * functions: {@link #findTrue5BSignal(int[])} to fill {@link #new5B}; {@link
+    * #accumulateBits(int)} to handle the repetitive bit-shifting; and {@link
+    * #printDecodedSignal(byte[])} to print the decoded signal.
+    * 
+    * @param received - the signal to decode, which are Bytes represented as
+    *          integers, which represent a 1 or 0 bit value.
+    * @return This returns the true 4b signal, decoded from the NRZI 4B/5B
+    *          signal.
+    */
+   public byte[] decode(int[] received)
+   {
+      // fills new5B
+      findTrue5BSignal(received);
+      byte[] true4BSignal = new byte[32];
+      // i traverses the new5B array, j traverses the resulting array
+      int i = 0;
+      int j = 0;
+      
+      while (i < new5B.length)
+      {
+         // Bit shift the left and right half of the Bytes separately
+         int left = accumulateBits(i);
+         i += 5;
+         int right = accumulateBits(i);
+         i += 5;
+         // So that we can look them up in the table separately
+         byte leftByte = table.get(left);
+         byte rightByte = table.get(right);
+         // And finally combine them to form 1 Byte
+         true4BSignal[j++] = (byte) ((leftByte << 4) + rightByte);
+      }
+      
+      // A simple printout of the signal.
+      printDecodedSignal(true4BSignal);
+      
+      return true4BSignal;
+   }
+
+   /**
+    * This method finds the true 5b signal from the original stream of Bytes and
+    * saves it in {@link #new5B}. It is not a one-to-one translation from the
+    * original5B to the new5B because it is encoded in NRZI.
+    * NRZI: If the PRevious signal was LOW, and the CUrrent signal is LOW, then
+    *   the ORiginal signal is LOW because we only switch the signal on HIGH. To
+    *   clarify, here is a cute little table:
+    *     PR  CU | OR 
+    *   ---------+-----
+    *     LO  LO | LO
+    *     LO  HI | HI
+    *     HI  LO | HI
+    *     HI  HI | LO
+    * This method is called in {@link #decode(int[])}.
+    * 
+    * @param original5B - the original stream of Bytes which are represented as
+    *          integers, which represent bits.
+    */
+   private void findTrue5BSignal(int[] original5B)
    {
       new5B = new int[original5B.length];
       // Given 320 bits,
       // convert bits into original 5b signal
-      /*
-       * Encoded in NRZI.
-       * If the previous signal was low and the current signal is low,
-       * this means that the original signal is low, since we only switch
-       * the signal on high, see below.
-       * Pr Cu Or
-       * LO LO LO
-       * LO HI HI
-       * HI LO HI
-       * HI HI LO
-       */
-      int previous = 0; // starts low
+
+      int previous = 0;
+      // This "previous" value is an educated guess. I checked the preamble many
+      // times to see how the signal leaves off (HI/LO?), and it seemed to leave
+      // off at low every time.
       for (int i = 0; i < original5B.length; ++i)
       {
          if (previous < baseline && original5B[i] < baseline)
@@ -107,26 +149,40 @@ public class Decoder
             // HI HI -> LO
             new5B[i] = 0;
          }
-         else
-         {
-            System.out.println("Possible error in NRZI conversion");
-         }
          
          previous = original5B[i];
       }
    }
    
-   public void printOG5B()
+   /**
+    * This method simply shifts bits around so that they can eventually be
+    * looked up. It is called in {@link #decode(int[])} twice, since it only
+    * pulls five bits together at a time.
+    * @param index - This corresponds to the values to pull together in the
+    *          array, {@link #new5B}.
+    * @return - returns an integer representing the group of 5 bits.
+    */
+   private int accumulateBits(int index)
    {
-      for (int i = 0; i < new5B.length; ++i)
+      int b = 0;
+      for (int bitShift = 4; bitShift >= 0; --bitShift)
       {
-         System.out.print(" " + new5B[i]);
+         b += new5B[index++] << bitShift;
+      }
+      return b;
+   }
+
+   /**
+    * This simply prints the decoded signal.
+    * @param bytes
+    */
+   private void printDecodedSignal(byte[] bytes)
+   {
+      System.out.print("Received 32 bytes: ");
+      for (int i = 0; i < bytes.length; ++i)
+      {
+         System.out.print(String.format("%02X", bytes[i]));
       }
       System.out.println();
-   }
-   
-   public void printTable()
-   {
-      System.out.println(table.toString());
    }
 }
--- CS380-P2/PhysLayerClient.java
+++ CS380-P2/PhysLayerClient.java
@@ -1,9 +1,23 @@
+/*
+ * Rachel Chiang
+ * CS 380.01 Computer Networks
+ * Project 2: 4B/5B NRZI
+ */
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * This class handles communication with the server. After connecting with the
+ * server, it receives the 64B preamble to establish a baseline. Then, it will
+ * accept 320 Bytes from the server, which represent a signal of 32 Bytes that
+ * has been encoded with NRZI and 4B/5B. It uses Decoder to decode the encoded
+ * 32 Bytes signal. Finally, the client transmits this decoded message back to
+ * the server. If the message was decoded properly, the server will respond 1;
+ * otherwise, it will respond 0.
+ */
 public class PhysLayerClient
 {
    public static void main(String[] args)
@@ -15,42 +29,31 @@ public class PhysLayerClient
          // Setting up streams
          OutputStream os = socket.getOutputStream();
          InputStream is = socket.getInputStream();
-         
+
          System.out.println("Connected to server.");
          
          // Establish baseline from the preamble of 64 HI/LO alternating bytes
          double baseline = 0;
          for (int i = 0; i < 64; ++i)
          {
-            //if (i+1 == 64)
-            //{
-            //   int last = is.read();
-            //   System.out.println("Last: " + last);
-            //   baseline += last;
-            //}
-            //else
-            //{
-               baseline += is.read();               
-            //}
+            baseline += is.read();
          }
          baseline = baseline/64.0;
          System.out.println("Baseline established from preamble: " + baseline);
-         
+
          // Receive 32B of randomly generated data
-         // Since it's encoded in 4b/5b, 32B=256b => 320 bits (for the +1b per 4b)
+         // Since it's encoded in 4b/5b,
+         //  32B=256b => 320 bits (for the +1b per 4b)
          int[] received = new int[320];
          for (int i = 0; i < 320; ++i)
          {
             received[i] = is.read();
-            //System.out.print(" " + String.format("%02X", received[i]));
-            System.out.print("  " + received[i]);
          }
-         System.out.println();
-         Decoder d = new Decoder(received, baseline);
+         Decoder d = new Decoder(baseline);
          // Decode from NRZI-encoded signal --> 5b signal
          // Decode from 5b --> 4b
          // Record this decoded 32B to an array of 32B
-         byte[] msg = d.decode();
+         byte[] msg = d.decode(received);
          
          // Send the 32B back to the server
          for (int i = 0; i < msg.length; ++i)
